1324e7e5c909a82146dd8ee1dc2fa71519e9bcd8
joxerx <n.lebedinsky@bk.ru>
2023-01-22 01:03:04 +0300

    Добавьте файлы проекта.

---
+++ versta24/Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace versta24.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+        public string Index()
+        {
+            return "This is my default action! Лежит в индексе";
+        }
+        //
+        // GET: /HelloWorld/Welcome/
+        public string Welcome()
+        {
+            return "This is the Welcome action method! В приветствии";
+        }
+    }
+}
