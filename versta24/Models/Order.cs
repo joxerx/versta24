ee798863e205efbb8f57f280dc01dd5129ee4bdf
joxerx <n.lebedinsky@bk.ru>
2023-01-22 19:03:45 +0300

    Теперь десятичные числа можно вводить через запятую

--- versta24/Models/Order.cs
+++ versta24/Models/Order.cs
@@ -8,33 +8,33 @@ namespace versta24.Models
     {
         [Display(Name = "Номер заказа #")]
         public int Id { get; set; }
-        
-        [Required]
+
+        [Required(ErrorMessage = "Обязательное поле")]
         [StringLength(30, MinimumLength = 4)]
         [Display(Name = "Город отправителя")]
         public string? DepartureCity { get; set; }
-        
-        [Required]
+
+        [Required(ErrorMessage = "Обязательное поле")]
         [StringLength(90, MinimumLength = 4)]
         [Display(Name = "Адрес отправителя")]
         public string? DepartureAddress { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Обязательное поле")]
         [StringLength(30, MinimumLength = 4)]
         [Display(Name = "Город получателя")]
         public string? RecipentCity { get; set; }
         
         [Display(Name = "Адрес получателя")]
-        [Required]
+        [Required(ErrorMessage ="Обязательное поле")]
         [StringLength(90, MinimumLength = 4)]
         public string? RecipentAddress { get; set; }
 
-        [Required]
-        
+        [Required(ErrorMessage = "Обязательное поле")]
+        [Range(typeof(double), "0,1", "10000,0", ErrorMessage = "Значение поля может принимать значения от 0,1 до 10000,0. Запятая в качестве разделителя")]
         [Display(Name = "Вес груза, кг")]
         public double Weight { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Обязательное поле")]
         [Display(Name = "Дата забора груза")]
         [DataType(DataType.Date)]
         public DateTime CollectionDate { get; set; }
--- versta24/wwwroot/lib/jquery-validation/dist/jquery.validate.js
+++ versta24/wwwroot/lib/jquery-validation/dist/jquery.validate.js
@@ -1406,8 +1406,7 @@ $.extend( $.validator, {
 
 		// https://jqueryvalidation.org/number-method/
 		number: function( value, element ) {
-			  return this.optional(element) || /^-?(?:\d+|\d{1,3}(?:[\s\.,]\d{3})+)(?:[\.,]\d+)?$/.test(value);
-			//return this.optional( element ) || /^(?:-?\d+|-?\d{1,3}(?:,\d{3})+)?(?:\.\d+)?$/.test( value );
+			return this.optional( element ) || /^(?:-?\d+|-?\d{1,3}(?:,\d{3})+)?(?:\.\d+)?$/.test( value );
 		},
 
 		// https://jqueryvalidation.org/digits-method/
@@ -1445,9 +1444,7 @@ $.extend( $.validator, {
 
 		// https://jqueryvalidation.org/range-method/
 		range: function( value, element, param ) {
-			var globalizedValue = value.replace(",", ".");
-			return this.optional(element) || (globalizedValue >= param[0] && globalizedValue <= param[1]);
-			//return this.optional( element ) || ( value >= param[ 0 ] && value <= param[ 1 ] );
+			return this.optional( element ) || ( value >= param[ 0 ] && value <= param[ 1 ] );
 		},
 
 		// https://jqueryvalidation.org/step-method/
