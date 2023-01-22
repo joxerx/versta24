e8aa1d384910d345bc6cb8d42cd2cfee7fe704cf
joxerx <n.lebedinsky@bk.ru>
2023-01-22 17:09:17 +0300

    Изменены отображения: изменены кнопки, добавлены тени, кликабельность строк таблицы + бг

--- versta24/Models/Order.cs
+++ versta24/Models/Order.cs
@@ -1,16 +1,24 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace versta24.Models
 {
     public class Order
     {
-        public int Id { get; set; }
+        [Display(Name = "Номер заказа #")]
+        public int Id { get; set; }    
+        [Display(Name = "Город отправителя")]
         public string? DepartureCity { get; set; }
+        [Display(Name = "Адрес отправителя")]
         public string? DepartureAddress { get; set; }
+        [Display(Name = "Город получателя")]
         public string? RecipentCity { get; set; }
+        [Display(Name = "Адрес получателя")]
         public string? RecipentAddress { get; set; }
+        [Display(Name = "Вес груза")]
         public double Weight { get; set; }
+        [Display(Name = "Дата забора груза")]
         [DataType(DataType.Date)]
         public DateTime CollectionDate { get; set; }
     }
