159acc2d0baa23191d47133970886f4a6ad51c8f
joxerx <n.lebedinsky@bk.ru>
2023-01-22 03:42:25 +0300

    Подготовка к миграции на .net 7

---
+++ versta24/Models/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace versta24.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime CreatedDate { get; set; }
+        public string DepartureCity { get; set; }
+        public string DepartureAddress { get; set; }
+        public string RecipentCity { get; set; }
+        public string RecipentAddress { get; set; }
+        public double Weight { get; set; }
+        public DateTime CollectionDate { get; set; }
+    }
+}
